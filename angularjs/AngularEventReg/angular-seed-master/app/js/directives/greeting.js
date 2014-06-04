04eb9f859dde46132c971a3cc86039f403dbbdd7
Dave Wilson <david.tosslehoff@gmail.com>
2014-06-04 22:48:13 +0100

    Require

--- angularjs/AngularEventReg/angular-seed-master/app/js/directives/greeting.js
+++ angularjs/AngularEventReg/angular-seed-master/app/js/directives/greeting.js
@@ -6,16 +6,39 @@ eventsApp
             restrict: 'E',
             replace: true,
             template: "<button class='btn' ng-click='sayHello()'>Say Hello</button>",
-            controller: '@',
-            name: 'ctrl'
+            controller: function($scope) {
+                var greetings = ['Hello'];
+                $scope.sayHello = function() {
+                    alert(greetings.join());
+                };
+                this.addGreeting = function(greeting) {
+                    greetings.push(greeting);
+                };
+            }
+        };
+    })
+    .directive('finnish', function() {
+        return {
+            restrict: 'A',
+            require: 'greeting',
+            link: function (scope, element, attrs, controller) {
+                controller.addGreeting('hei');
+            }
+        };
+    })
+    .directive('hindi', function() {
+        return {
+            restrict: 'A',
+            require: 'greeting',
+            link: function (scope, element, attrs, controller) {
+                controller.addGreeting('some stuff');
+            }
         };
     });
 
 eventsApp.controller('GreetingsController',
     function GreetingsController($scope) {
-        $scope.sayHello = function() {
-            alert("Hello!");
-        }
+
     });
 
 
