4e05e4731fe076353940b731b3d5bd6223faa184
Dave Wilson <david.tosslehoff@gmail.com>
2014-06-04 21:23:38 +0100

    Isolate Scope

--- angularjs/AngularEventReg/angular-seed-master/app/js/directives/eventThumbnail.js
+++ angularjs/AngularEventReg/angular-seed-master/app/js/directives/eventThumbnail.js
@@ -2,9 +2,12 @@
 
 eventsApp.directive('eventThumbnail', function() {
 
-   return {
-    restrict: 'E',
-    replace: true,
-    templateUrl: '/templates/directives/eventThumbnail.html'};
-
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: '/templates/directives/eventThumbnail.html',
+        scope: {
+            event: "="
+        }
+    };
 });
