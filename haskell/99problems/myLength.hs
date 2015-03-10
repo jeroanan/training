9251253a90d4d3c28770dac5b651d531156940e2
Dave Wilson <david.tosslehoff@gmail.com>
2015-03-10 14:06:07 +0000

    Mylength with foldr+annotation

--- haskell/99problems/myLength.hs
+++ haskell/99problems/myLength.hs
@@ -1,8 +1,13 @@
-myLength :: [a] -> Int -> Int
-myLength [] c = c
-myLength (x:xs) c = myLength xs (succ c)
+myLength :: [a] -> Int
+myLength [] = 0
+myLength x = (fst . last) $ zip [1..] x
+
+myLength2 :: [a] -> Int
+--                 Function    Starting val, list (point-free here)
+myLength2 = foldr (const (+1)) 0
 
 main :: IO()
 main = do
-  putStrLn $ show $ myLength [123, 456, 789] 0
-  putStrLn $ show $ myLength "Hello, world!" 0
+  putStrLn $ show $ myLength2 [123, 456, 789]
+  putStrLn $ show $ myLength2 "Hello, world!"
+  putStrLn $ show $ myLength2 ""
